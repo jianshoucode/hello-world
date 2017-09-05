6d0c7ed3e2df4db24813d3759f9dd7ca96a7ad08
jianshoucode <lpl89cslg@163.com>
2017-09-05 17:43:01 +0800

    test3

--- com.jianshoucode.test/src/com/jianshoucode/test/Test.java
+++ com.jianshoucode.test/src/com/jianshoucode/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 		System.out.println("hello world2!");
+		//https://github.com/jianshoucode/hello-world.git
 	}
 
 }
