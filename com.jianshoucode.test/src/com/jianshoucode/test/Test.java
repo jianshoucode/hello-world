4d5496b8dd533882c17e68f2fb0f1a12a90e3f2f
jianshoucode <lpl89cslg@163.com>
2017-09-05 17:32:04 +0800

    edit5

---
+++ com.jianshoucode.test/src/com/jianshoucode/test/Test.java
@@ -0,0 +1,10 @@
+package com.jianshoucode.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		System.out.println("hello world2!");
+	}
+
+}
